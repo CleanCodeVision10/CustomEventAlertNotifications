e05d0b986331fd419a2a385556f76e6f62976633
Arish Sharma <aarish3521@gmail.com>
2025-07-25 14:39:05 +0000

    Details Added

---
+++ btp-cockpit-ans-configuration/readme.md
@@ -0,0 +1,1 @@
+Type of service key should be OAUTH before creation. Add this as json configuration. For exact code refer documentation
--- srv/cat-service.js
+++ srv/cat-service.js
@@ -8,10 +8,10 @@ const {
 const CatalogService = (srv) => {
         srv.on('createEvent', async req => {
 
-
+            // Dummy client with invalid Credentials. Please replace it alert notification service with your instance key details
             const client = new AlertNotificationClient({
                 authentication: new OAuthAuthentication({
-                    username: 'sb-7c26653c-4aa7-45d1-8df7-87b1f1ded477!b478401|ans-xsuaa!b673',
+                    username: 'sb-7c26653c-4aa7-45d1-8df7-87b1f1ded477!b478401|ans-xsuaa!b673', 
                     password: '8882ace7-b65c-4867-918f-c4a7b7adcbe7$kG03aokrw7atJE9YyxwCp5bUbLBHbNTQ2_Y8XCljMT4=',
                     oAuthTokenUrl: 'https://6c05d52ctrial.authentication.us10.hana.ondemand.com/oauth/token?grant_type=client_credentials'
                 }),
